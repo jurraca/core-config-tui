3702bdba15cbb80ebeb3557460f191043ddd89ae
jurraca <jurraca@users.noreply.github.com>
2025-01-27 19:50:28 +0000

    Added form inputs for Bitcoin Core options

--- main.go
+++ main.go
@@ -33,6 +33,28 @@ var (
 	prune       string
 )
 
+var (
+	datadir          string
+	network          string
+	server           bool
+	includeConf      string
+	loadBlock        string
+	maxMempool       string
+	maxOrphanTx      string
+	mempoolExpiry    string
+	par              string
+	persistMempool   bool
+	persistMempoolV1 bool
+	pid              string
+	prune            string
+	reindex          bool
+	reindexChainstate bool
+	settings         string
+	shutdownNotify   string
+	startupNotify    string
+	txindex          bool
+)
+
 func main() {	
 	form := huh.NewForm(
 		huh.NewGroup(
@@ -50,12 +72,87 @@ func main() {
 					huh.NewOption("regtest", "regtest"),
 					huh.NewOption("signet", "signet"),
 				).
-			  Value(&network),
+			  	Value(&network),
+
+			huh.NewInput().
+				Title("Include Config").
+				Description("Specify additional configuration file, relative to the -datadir path").
+				Value(&includeConf),
+
+			huh.NewInput().
+				Title("Load Block").
+				Description("Imports blocks from external file on startup").
+				Value(&loadBlock),
+
+			huh.NewInput().
+				Title("Max Mempool").
+				Description("Keep the transaction memory pool below <n> megabytes (default: 300)").
+				Value(&maxMempool),
+
+			huh.NewInput().
+				Title("Max Orphan Transactions").
+				Description("Keep at most <n> unconnectable transactions in memory (default: 100)").
+				Value(&maxOrphanTx),
+
+			huh.NewInput().
+				Title("Mempool Expiry").
+				Description("Do not keep transactions in the mempool longer than <n> hours (default: 336)").
+				Value(&mempoolExpiry),
+
+			huh.NewInput().
+				Title("Script Verification Threads").
+				Description("Set the number of script verification threads (0 = auto, up to 15, <0 = leave that many cores free, default: 0)").
+				Value(&par),
 
 			huh.NewConfirm().
-				Title("Enable Server?").
-				Description("Accept command-line and JSON-RPC commands").
-				Value(&server),
+				Title("Persist Mempool").
+				Description("Whether to save the mempool on shutdown and load on restart (default: 1)").
+				Value(&persistMempool),
+
+			huh.NewConfirm().
+				Title("Use Legacy Mempool Format").
+				Description("Whether a mempool.dat file will be written in the legacy format (version 1) or current format (version 2)").
+				Value(&persistMempoolV1),
+
+			huh.NewInput().
+				Title("PID File").
+				Description("Specify pid file (default: bitcoind.pid)").
+				Value(&pid),
+
+			huh.NewInput().
+				Title("Prune Blockchain").
+				Description("Reduce storage by pruning old blocks (>=550 MB to retain)").
+				Value(&prune),
+
+			huh.NewConfirm().
+				Title("Reindex").
+				Description("Rebuild chain state and block index from blk*.dat files").
+				Value(&reindex),
+
+			huh.NewConfirm().
+				Title("Reindex Chainstate").
+				Description("Rebuild chain state from block files").
+				Value(&reindexChainstate),
+
+			huh.NewInput().
+				Title("Settings File").
+				Description("Path to dynamic settings data file (default: settings.json)").
+				Value(&settings),
+
+			huh.NewInput().
+				Title("Shutdown Notify Command").
+				Description("Execute command before shutdown").
+				Value(&shutdownNotify),
+
+			huh.NewInput().
+				Title("Startup Notify Command").
+				Description("Execute command on startup").
+				Value(&startupNotify),
+
+			huh.NewConfirm().
+				Title("Transaction Index").
+				Description("Maintain a full transaction index").
+				Value(&txindex),
 					
 			huh.NewInput().
 				Title("RPC Username").
