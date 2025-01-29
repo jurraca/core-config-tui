0c8462a47aa9484d335600f3da68684e03125775
jurraca <jurraca@users.noreply.github.com>
2025-01-29 17:04:39 +0000

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+
+{
+  description = "Bitcoin Core configuration generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "bitcoin-core-config";
+          version = "0.1.0";
+          src = ./.;
+          
+          vendorHash = "sha256-bTV7RQ1An26kDSTGQf1lm5Jai2yGuV6NfZMMiO/isZs=";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+          ];
+        };
+      });
+}
