b76196a14143e917435fd75824081a0473fc828e
jurraca <julienu@pm.me>
2025-02-21 21:14:52 +0000

    format nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Bitcoin Core configuration generator";
 
@@ -7,25 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.buildGoModule {
-          pname = "bitcoin-core-config";
-          version = "0.1.0";
-          src = ./.;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.buildGoModule {
+        pname = "bitcoin-core-config";
+        version = "0.1.0";
+        src = ./.;
 
-          vendorHash = "sha256-bTV7RQ1An26kDSTGQf1lm5Jai2yGuV6NfZMMiO/isZs=";
-        };
+        vendorHash = "sha256-bTV7RQ1An26kDSTGQf1lm5Jai2yGuV6NfZMMiO/isZs=";
+      };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-            gopls
-          ];
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+        ];
+      };
+    });
 }
